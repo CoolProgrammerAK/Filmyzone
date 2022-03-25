a9e2bccca92e045bccc7402f11777b8b427c3913
CoolProgrammerAK <avineykhetarpal01@gmail.com>
2022-03-25 11:10:14 +0530

    initial commit2

--- router/router.js
+++ router/router.js
@@ -9,7 +9,7 @@ const router=express.Router()
 var url=process.env.MOVIEURL
 router.get("/latest/:page",async(req,res)=>{
      try {
-          var result=await page(req.params.page,url)
+          var result=await page(req.params.page,process.env.MOVIEURL)
           return res.json({result})
      } catch (error) {   
         return  res.status(500).json({error:"Something went wrong"})
@@ -18,7 +18,7 @@ router.get("/latest/:page",async(req,res)=>{
 
 router.get("/category/:category/page/:page",async(req,res)=>{
      try {
-          var result=await latest(url,req.params.category,req.params.page)
+          var result=await latest(process.env.MOVIEURL,req.params.category,req.params.page)
          return res.json({result})
      } catch (error) {
         return  res.status(500).json({error:"Something went wrong"})
@@ -29,7 +29,7 @@ router.get("/category/:category/page/:page",async(req,res)=>{
 router.get("/search/q=:name",async(req,res)=>{
 
      try {
-          var result=await search_movie(req.params.name,url,req.params.page)
+          var result=await search_movie(req.params.name,process.env.MOVIEURL,req.params.page)
          return res.json({result})
      } catch (error) {
          return res.status(500).json({error:"Something went wrong"})
@@ -38,7 +38,7 @@ router.get("/search/q=:name",async(req,res)=>{
 
 router.post("/movie-description",async(req,res)=>{
      try {
-          var result=await get_details(req.body.link,url)
+          var result=await get_details(req.body.link,process.env.MOVIEURL)
          return res.json({result})
      } catch (error) {
         return  res.status(500).json({error:"Something went wrong"})
@@ -48,7 +48,7 @@ router.post("/movie-description",async(req,res)=>{
 router.post("/download",async(req,res)=>{
      try {
   
-          var result=await download_movie(req.body.fname,req.body.fsip,url)
+          var result=await download_movie(req.body.fname,req.body.fsip,process.env.MOVIEURL)
         return  res.json({result})
      } catch (error) {
         return  res.status(500).json({error:"Something went wrong"})
